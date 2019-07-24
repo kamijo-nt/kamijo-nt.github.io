c3677586c1bb55cd9b47740b98f403220d65abac
kamijo-nt <kamijo.nt@gmail.com>
2019-07-24 15:58:37 +0900

    fix

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,19 @@
 module.exports = {
   title: 'Kamijo Notes',
   description: '勉強記録',
+  locales: {
+    '/': {
+      lang: 'ja'
+    }
+  },
+  meta: [{
+      charset: 'utf-8'
+    },
+    {
+      name: 'viewport',
+      content: 'width=device-width, initial-scale=1'
+    }
+  ],
   themeConfig: {
     sidebar: 'auto'
   },
